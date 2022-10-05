fa815c27f316f3796ba96bd49e7f37dc6b52795d
MrJones702 <jones.maurice39@yahoo.com>
2022-10-05 00:00:53 -0700

    Track Components

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Track.css';
+
+export default class Track extends React.Component {
+  render() {
+
+    renderAction() {
+        if(isRemoval){
+            return <button className="Track-action">-</button>;
+        } else {
+            return <button className="Track-action">+</button>
+        }
+    }
+
+    return (
+        <div className="Track">
+        <div className="Track-information">
+          {/* <h3><!-- track name will go here --></h3> */}
+          {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+        </div>
+        {/* <button class="Track-action"><!-- + or - will go here --></button> */}
+      </div>
+    )
+  }
+}
