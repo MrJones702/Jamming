13f34524aa7dd283ae4c2fd147d6873802ced0f9
MrJones702 <jones.maurice39@yahoo.com>
2022-10-06 23:29:31 -0700

    pass Tracklist state to Track.

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,24 +2,29 @@ import React from 'react'
 import './Track.css';
 
 export default class Track extends React.Component {
-  render() {
+  // constructor(props){
+  //   super(props);
 
+  //   renderAction = renderAction.bind(this);
+  // }
+  
     renderAction() {
-        if(isRemoval){
+        if(this.props.isRemoval) {
             return <button className="Track-action">-</button>;
         } else {
             return <button className="Track-action">+</button>
         }
     }
 
-    return (
+    render() {
+      return (
         <div className="Track">
-        <div className="Track-information">
-          {/* <h3><!-- track name will go here --></h3> */}
-          {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+          <div className="Track-information">
+            <h3>track name will go here </h3>
+            <p>track artist | track album </p>
+          </div>
+          {this.renderAction()}
         </div>
-        {/* <button class="Track-action"><!-- + or - will go here --></button> */}
-      </div>
-    )
-  }
+      )
+    }
 }
